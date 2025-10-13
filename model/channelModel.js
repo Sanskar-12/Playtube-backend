f97e3c5bbc1c8695e22164de1a538684460d9292
Sanskar-12 <sanskarv2004@gmail.com>
2025-10-13 21:38:48 +0530

    added Channel model

---
+++ model/channelModel.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+const channelSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      red: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    banner: {
+      type: String,
+      default: "",
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+    subscribers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    videos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    shorts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Short",
+      },
+    ],
+    playlists: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Playlist",
+      },
+    ],
+    communityPosts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Channel = mongoose.model("Channel", channelSchema);
