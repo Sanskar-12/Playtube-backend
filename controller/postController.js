7dbce2f6ad7fe72f68b7451cbac5f7ad07015120
Sanskar-12 <sanskarv2004@gmail.com>
2025-11-09 18:09:41 +0530

    added api for adding comments and replies in post and toggle likes in post

--- controller/postController.js
+++ controller/postController.js
@@ -81,3 +81,142 @@ export const getAllPosts = async (req, res) => {
     });
   }
 };
+
+export const togglePostLikes = async (req, res) => {
+  try {
+    const { postId } = req.body;
+
+    const userId = req.user._id;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    if (post.likes.includes(userId)) {
+      post.likes = post.likes.filter(
+        (like) => like._id.toString() !== userId.toString()
+      );
+    } else {
+      post.likes.push(userId);
+    }
+
+    await post.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Post liked successfully",
+      post,
+    });
+  } catch (error) {
+    console.log("Error in toggle post likes", error);
+    return res.status(500).json({
+      success: false,
+      message: `togglePostLikes Error: ${error}`,
+    });
+  }
+};
+
+export const addPostComment = async (req, res) => {
+  try {
+    const { postId, message } = req.body;
+    const userId = req.user._id;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    post.comments.unshift({
+      author: userId,
+      message,
+    });
+
+    await post.save();
+
+    const populatedPost = await Post.findById(postId)
+      .populate({
+        path: "comments.author",
+        select: "userName photoUrl email",
+      })
+      .populate({
+        path: "comments.replies.author",
+        select: "userName photoUrl email",
+      });
+
+    return res.status(200).json({
+      success: true,
+      message: "Comment added successfully",
+      video: populatedPost,
+    });
+  } catch (error) {
+    console.log("Error in add Post Comments", error);
+    return res.status(500).json({
+      success: false,
+      message: `addPostComment Error: ${error}`,
+    });
+  }
+};
+
+export const addPostReply = async (req, res) => {
+  try {
+    const { postId, commentId, message } = req.body;
+
+    const userId = req.user._id;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    const comment = post.comments.id(commentId);
+
+    if (!comment) {
+      return res.status(404).json({
+        success: false,
+        message: "Comment not found",
+      });
+    }
+
+    comment.replies.unshift({
+      author: userId,
+      message,
+    });
+
+    await post.save();
+
+    const populatedPost = await Post.findById(postId)
+      .populate({
+        path: "comments.author",
+        select: "userName photoUrl email",
+      })
+      .populate({
+        path: "comments.replies.author",
+        select: "userName photoUrl email",
+      });
+
+    return res.status(200).json({
+      success: true,
+      message: "Reply added successfully",
+      video: populatedPost,
+    });
+  } catch (error) {
+    console.log("Error in add post Reply", error);
+    return res.status(500).json({
+      success: false,
+      message: `addPostReply Error: ${error}`,
+    });
+  }
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,11 +1,20 @@
 import express from "express";
 import { isAuth } from "../middleware/isAuth.js";
 import upload from "../middleware/multer.js";
-import { createPost, getAllPosts } from "../controller/postController.js";
+import {
+  addPostComment,
+  addPostReply,
+  createPost,
+  getAllPosts,
+  togglePostLikes,
+} from "../controller/postController.js";
 
 const router = express.Router();
 
 router.post("/create/post", isAuth, upload.single("image"), createPost);
 router.get("/get/all/posts", isAuth, getAllPosts);
+router.put("/toggle/post/likes", isAuth, togglePostLikes);
+router.post("/add/post/comment", isAuth, addPostComment);
+router.post("/add/post/reply", isAuth, addPostReply);
 
 export default router;
