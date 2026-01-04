311cb30239c95ae6318e194a6c6f558975efe603
Sanskar-12 <sanskarv2004@gmail.com>
2026-01-04 12:05:52 +0530

    Fixed bug for token in prod

--- controller/authController.js
+++ controller/authController.js
@@ -53,8 +53,8 @@ export const signUp = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "Strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
