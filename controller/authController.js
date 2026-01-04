dbfecc09c183847bbe4027269dd453c0cf37104c
Sanskar-12 <sanskarv2004@gmail.com>
2026-01-04 12:12:22 +0530

    token bug fixed in signin

--- controller/authController.js
+++ controller/authController.js
@@ -100,8 +100,8 @@ export const signIn = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "Strict",
-      secure: false,
+      sameSite: "None",
+      secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
